753f70cc2aff84f3782e4c5983634fe64e970a0b
virtouso <s_moeen_kamali@yahoo.com>
2023-04-01 04:01:04 +0430

    more lifetime

--- rust/moeenTest/src/main.rs
+++ rust/moeenTest/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    let moeen: i32 = 232;
+    println!("please input you shitty guess{}", moeen);
+    println!("this is new data::{}", new_method(10));
 }
+
+fn fair_dice_roll() -> i32 {
+    return 4;
+}
+
+fn new_method(x: i32) -> i32 {
+    x * 2
+}
+
+
